65166f42631d0a7ddcfaa168340de37fcc1b4d75
Shivadarshan-HM <25lbcs3116@culkomail.in>
2026-06-24 16:40:42 +0530

    sitemap is fixed

---
+++ src/app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://mahaveer-electronics-and-furniture.vercel.app/sitemap.xml',
+  }
+}
